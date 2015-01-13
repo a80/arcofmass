3e4460f29e3094fc698f6bf559aff3654d819ec2
Evan Denmark <edenmark@mit.edu>
2015-01-13 10:11:17 -0800

    Changed named from profile to settings.

---
+++ user-settings/settings_page.js
@@ -0,0 +1,56 @@
+if (Meteor.IsClient){
+	Template.body.event({
+		"submit .name-enter": function(event) {
+
+		var text = event.target.text.value;
+
+		Tasks.insert({
+			text: text,
+			
+		});
+
+		//Clears the form
+		event.target.text.value = "";
+	
+		//prevent default form submit
+		return false;
+		}
+	});
+
+	Template.body.event({
+		"submit .email-enter": function(event) {
+
+		var text = event.target.text.value;
+
+		Tasks.insert({
+			text: text,
+			
+		});
+
+		//Clears the form
+		event.target.text.value = "";
+	
+		//prevent default form submit
+		return false;
+		}
+	});
+
+
+	Template.body.event({
+		"submit .story-enter": function(event) {
+
+		var text = event.target.text.value;
+
+		Tasks.insert({
+			text: text,
+			
+		});
+
+		//Clears the form
+		event.target.text.value = "";
+	
+		//prevent default form submit
+		return false;
+		}
+	});
+}
