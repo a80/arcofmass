639e7d29d0bb6f955efc7a3770af8dc80cb70a51
jeaniep <jeaniep@mit.edu>
2015-01-14 15:15:44 -0500

    Update getUserIssues to sort

--- client/helpers.js
+++ client/helpers.js
@@ -1,8 +1,11 @@
 Meteor.methods({
   getUserIssues: function() {
 	//Returns the names of the users issues
-	return Meteor.user.issues;
-  }, 
+    return Meteor.user.issues.sort(function(a, b) {
+        var x = a[count]; var y = b[count];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}, 
   increaseIssueCount: function(issue) {
 	//Increases issue count by one
 	//For use when a user adds an issue that they now care about
@@ -20,7 +23,7 @@ Meteor.methods({
   insertNewIssue: function(name) {
 	//When a new issue has been added, insert it into the issues collection
 	issues.insert({name: name, count: 0, important: false});
-  }
+  },
   toggleIssueImportance: function(myName) {
 	//When the issue radio button is clicked, the issue boolean is updated
 	var toUpdate = issues.find({name: myName});
