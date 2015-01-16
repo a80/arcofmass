57e1ae1ccfa93a811cd197f12a1b836dac520709
jeaniep <jeaniep@mit.edu>
2015-01-15 22:00:04 -0500

    Update publish and subscribe

--- collections/collections.js
+++ collections/collections.js
@@ -9,3 +9,9 @@ issues = new Meteor.Collection("issues"); //redefined in profile-home.js
 Meteor.publish('userIssues', function() {
 	return issues.find();
 });
+Meteor.publish('legislatorsColl', function() {
+	return legislators.find();
+});
+Meteor.publish('actionsColl', function() {
+	return actionItems.find();
+});
