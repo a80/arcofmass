db34da06d99caa766a8960c9fc0dcfe2218f8d6c
Evan Denmark <edenmark@mit.edu>
2015-01-19 17:47:56 -0800

    made edit buttons initially invisible

--- user-settings/client/settings_page.js
+++ user-settings/client/settings_page.js
@@ -3,22 +3,74 @@
 if (!Roles.userIsInRole(Meteor.user(), ['admin'])) {
 	Meteor.subscribe("userSettingsData");
 	Template.userSettings.events({
-		"click #saveButton": function(event) {
-			var myZip = document.getElementById("inputZipCode").value;
-			var myStory = document.getElementById("inputStory").value;
-			var myName = document.getElementById("inputName").value;
-			var myEmail = document.getElementById("inputEmail").value;
-			var myOPassword = document.getElementById("inputOPassword").value;
-			var myNPassword = document.getElementBYId("inputNPassword").value;
-			
-			console.log("submitted.");
-
-			Meteor.call("modifyUser", myName, myZip, myStory, myEmail, myOPassword, myNPassword);
-		},
+		
 		
 		'click #user-home-button': function() {
     		Router.go('/profile');
   		},
+
+  		"click #saveNameButton": function(event){
+			document.getElementById("saveNameButton").visibility = "hidden";
+			document.getElementById("editNameButton").visibility = "visible";
+			document.getElementById("inputName").readOnly = true;
+
+		},
+
+		"click #editNameButton": function(event) {
+			document.getElementById("saveNameButton").visibility = "visible";
+			document.getElementById("editNameButton").visibility = "hidden";
+			document.getElementById("inputName").readOnly = false;
+		},
+
+		"click #saveEmailButton" : function(event) {
+			document.getElementById("saveEmailButton").visibility = "hidden";
+			document.getElementById("editEmailButton").visibility = "visible";
+			document.getElementById("inputEmail").readOnly = true;
+		},
+
+		"click #editEmailButton" : function(event){
+			document.getElementById("saveEmailButton").visibility = "visible";
+			document.getElementById("editEmailButton").visibility = "hidden";
+			document.getElementById("inputEmail").readOnly = false;
+
+		},
+
+		"click #saveStoryButton" : function(event){
+			document.getElementById("saveStoryButton").visibility = "hidden";
+			document.getElementById("editStoryButton").visibility = "visible";
+			document.getElementById("inputStory").readOnly = true;
+		},
+
+		"click #editStoryButton" : function(event){
+			document.getElementById("saveStoryButton").visibility = "visible";
+			document.getElementById("editStoryButton").visibility = "hidden";
+			document.getElementById("inputStory").readOnly = false;
+
+		},
+
+		"click #saveZipCodeButton" : function(event){
+			document.getElementById("saveZipCodeButton").visibility = "hidden";
+			document.getElementById("editZipCodeButton").visibility = "visible";
+			document.getElementById("inputZipcode").readOnly = true;
+		},
+
+		"click #editZipCodeButton" : function(event){
+			document.getElementById("saveZipCodeButton").visibility = "visible";
+			document.getElementById("editZipCodeButton").visibility = "hidden";
+			document.getElementById("inputZipcode").readOnly = false;
+		},
+
+		"click #savePasswordButton" : function(event) {
+			if (document.getElementById("inputNPassword").value === document.getElementById("confirmNPassword").value){
+			document.getElementById("inputOPassword").readOnly = true;
+			document.getElementById("inputNPassword").readOnly = true;
+			document.getElementById("confirmNPassword").readOnly = true;
+			}
+			else{
+				document.getElementById("alertMessage").visibility = "visible";
+			}
+
+		},
 	});
 
 
@@ -119,3 +171,4 @@ if (!Roles.userIsInRole(Meteor.user(), ['admin'])) {
 } else {
 	throw new Meteor.Error('403', 'permission denied');
 }
+
