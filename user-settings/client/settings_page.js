47b62c9187d1c729e79263fe5412a2b2dd1bfa9d
Evan Denmark <edenmark@mit.edu>
2015-01-19 18:50:52 -0800

    reinstated the save JS

--- user-settings/client/settings_page.js
+++ user-settings/client/settings_page.js
@@ -3,7 +3,18 @@
 if (!Roles.userIsInRole(Meteor.user(), ['admin'])) {
 	Meteor.subscribe("userSettingsData");
 	Template.userSettings.events({
-		
+		"click #saveButton": function(event) {
+			var myZip = document.getElementById("inputZipCode").value;
+			var myStory = document.getElementById("inputStory").value;
+			var myName = document.getElementById("inputName").value;
+			var myEmail = document.getElementById("inputEmail").value;
+			var myOPassword = document.getElementById("inputOPassword").value;
+			var myNPassword = document.getElementById("inputNPassword").value;
+			
+			console.log("submitted.");
+
+			Meteor.call("modifyUser", myName, myZip, myStory, myEmail, myOPassword, myNPassword);
+		},
 		
 		'click #user-home-button': function() {
     		Router.go('/profile');
