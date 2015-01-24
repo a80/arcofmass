8f3dbbb6d00dc8f6443e904dbea4629c32a26307
Eric Lau <eric_l@mit.edu>
2015-01-24 13:46:55 -0500

    Implement graph, grid view for admin home.

--- admin-home/client/admin-home.js
+++ admin-home/client/admin-home.js
@@ -1,26 +1,57 @@
 //<<<<<<< HEAD
 Template.adminHome.helpers({
-	getUserIssues: function() {
-/*=======
-if (Roles.userIsInRole(Meteor.user(), ['admin'])) {
-	Meteor.subscribe("adminHomeIssues");
-	Meteor.subscribe("adminHomeActions");
-	
-	Template.adminHome.helpers({
-	  getUserIssues: function() {
->>>>>>> 2c343a9573a36d97e0b1269cd795c61c0276640b*/
-		//Meteor.call("getUserIssues");
-		//var userIssues =  
-		//console.log("function entered: " + userIssues); 
-		console.log(issues.find({}).fetch());
-		return issues.find({}); 
-		//return ["first issue", "second issue", "third issue"]; 
-	},
-
 	returnUserId: function() {
 		return Meteor.user().username; 
 	},
+
+	toggleListViewDefault: function() {
+		if (Session.get("toggleListView") === undefined) {
+			return true; 
+		} else {
+			return Session.get("toggleListView"); 
+		}
+	}, 
+
+	returnListActive: function() {
+		var className; 
+		
+		if (Session.get("toggleListView") === undefined) {
+			className = "active"; 
+		} else {
+			if (Session.get("toggleListView")) {
+				className = "active"; 
+			} else {
+				className = ""; 
+			} 
+		}
+		return className; 
+	}, 
+
+	returnGraphActive: function() {
+		var className; 
+		
+		if (Session.get("toggleListView") === undefined) {
+			className = ""; 
+		} else {
+			if (Session.get("toggleListView")) {
+				className = ""; 
+			} else {
+				className = "active"; 
+			} 
+		}
+		return className;
+	}
+
+
+
 });
+
+Template.adminIssueListView.helpers({
+	getUserIssues: function() {
+		console.log(issues.find({}).fetch());
+		return issues.find({});  
+	},
+})
 
 Template.adminIssuePanel.helpers({
 	returnIssueName: function() {
@@ -58,13 +89,116 @@ Template.adminHome.events({
 	"click #update-button": function(event) {
 		Router.go("/update"); 
 	},
-
-	"click #region-button": function(event) {
-		Router.go("/region"); 
-	},
 	"click #logoutButton": function(event) {
 		Meteor.logout();
 		Router.go("/"); 
+	}, 
+	"click #toggleListView": function() {
+		Session.set("toggleListView", true);
+	},
+	"click #toggleGraphView": function() {
+		Session.set("toggleListView", false);
 	}
 });
+
+
+//adding graphs to adminHome
+
+var graphs = {};
+
+Template.adminIssueGraphView.rendered = function() {
+
+	Deps.autorun(function() {
+	var issueList = issues.find({}).fetch();
+    //var notifications = notifications.find({issueId: this._id}, {sort: {dateCompleted: -1}, limit: 3}); 
+
+    console.log(notifications); 
+    
+    _.each(issueList, function(issue) {
+      var graphID = issue.name.replace(/\s*/g, '');
+      graphs[graphID] = progressBar(".adminHomeGraphDiv", issue.count, "what: " + issue.name);
+    }); 
+	});
+
+  //how to vary other elements in progressBar based on function arguments. 
+
+}
+
+
+function progressBar(el, data, label) {
+ 	var self = this;
+	var canvas; 
+
+  	var width = 950; 
+  	var height = 400;
+
+  	var widthScale = d3.scale.linear()
+                      .domain([0, 20])
+                      .range([0, height]); 
+
+  	var color = d3.scale.linear()
+                .domain([0, 10])
+                .range(["red", "blue"]);
+
+    var yAxis = d3.svg.axis()
+    				.scale(d3.scale.linear().domain([0, 20]).range([height, 0]))
+    				.orient("left")
+    				.ticks(5); 
+
+
+  	var createCanvasSvg = function(el) {
+      d3.select(el)
+        .selectAll("svg")
+        .remove();
+        
+      	canvas = d3.select(el)
+                  .append("svg")
+                  .attr("width", width)
+                  .attr("height", height)
+                  .append("g")
+                  .attr("transform", "translate(0, 50)"); 
+  }
+
+  	createCanvasSvg(el);
+
+  	/*canvas.attr("class", "axis")
+  			.attr("transform", "translate(" + padding + ",0)")
+    		.call(yAxis);*/
+
+  	/*var bars = canvas.selectAll("rect")
+  					.data([20, 40, 50])
+  					.enter()
+  						.append("rect")
+  						.attr("width", function(d) {return d; })
+  						.attr("height", 50)
+  						.attr("y", function(d, i) {return i*100; });*/
+
+  	var bars = canvas.selectAll("rect")
+                  .data([10, 20, 30, 40, 50])
+                  .enter()
+                    .append("rect")
+                    .attr("y", 600)
+                    .attr("width", 100)
+                    .attr("fill", "orange")
+                    .transition()
+                    .duration(1000)
+                    .attr("height", function (d) { return widthScale(d); })
+                    .attr("y", function(d, i) {return height - widthScale(d) })
+                    .attr("x", function(d, i) {return 100 + (i*120)}); 
+                    //.attr("fill", function(d) {return color(d)});
+
+    canvas.append("g")
+    	.attr("transform", "translate(100, 0)")
+    	.call(yAxis);
+
+
+
+
+
+
+  
+  	//canvas.append('text').text(label).attr("x", 30).attr("y", 100).attr("fill", "white").style("font-size", "100px");
+
+}
+
 
