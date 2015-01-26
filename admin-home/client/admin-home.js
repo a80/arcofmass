bbc403607a314a819b8a063d036c479370a414d2
jeaniep <jeaniep@mit.edu>
2015-01-26 18:51:07 -0500

    Add percentage completion to todos on admin home

--- admin-home/client/admin-home.js
+++ admin-home/client/admin-home.js
@@ -72,7 +72,9 @@ Template.adminIssuePanel.helpers({
 		for (var i = 0; i < actionItemsData.length; i++) {
 		  //console.log("entered if loop"); 
 		  if (actionItemsData[i].issue === issueName) {
-			actionItemsForIssue.push(actionItemsData[i]); 
+			var toDo = actionItemsData[i];
+			var percent = parseInt((toDo.count / parseInt(toDo.goal))*100);
+			actionItemsForIssue.push(toDo.name + ": " + percent + "%"); 
 		  }
 		}
 
